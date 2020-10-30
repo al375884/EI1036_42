7c6d8bfe701eeffafc78377c068697d54400bb45
unknown <al375884@uji.es>
2020-10-30 20:21:59 +0100

    arreglo cesta vacia (no aparece el mensaje)

--- Proyecto/includes/listar_cesta.php
+++ Proyecto/includes/listar_cesta.php
@@ -2,21 +2,26 @@
 
 function listar_cesta()
 {
-    echo "<ul>";
-    #$comprar = '?action=realizar_compra';
-    foreach($_SESSION["cesta"] as $k => $producto){
-        if(0<strlen($producto)){
-            $eliminar = '?action=desencestar_producto&item_id='.$producto;
-            $comprar = '?action=realizar_compra&item_id='.$producto;    
-            echo "<li>".$producto . "\t <a href= ".$eliminar .">Eliminar</a> \t <a href=" .$comprar .">Comprar</a>";
-            #echo "<li>".$producto . "<form action=".$eliminar."\" method=\"POST\"> <input id=\"eliminar\" type=\"submit\" value=\"Eliminar\"> </input> </form> <form action=".$comprar."\" method=\"POST\"> <input id=\"comprar\" type=\"submit\" value=\"Comprar\"></input> </form>";
+    if(0<=count($_SESSION["cesta"])){
+        echo "<ul>";
+        #$comprar = '?action=realizar_compra';
+        foreach($_SESSION["cesta"] as $k => $producto){
+            if(0<=strlen($producto)){
+                $eliminar = '?action=desencestar_producto&item_id='.$producto;
+                $comprar = '?action=realizar_compra&item_id='.$producto;    
+                echo "<li>".$producto . "\t <a href= ".$eliminar .">Eliminar</a> \t <a href=" .$comprar .">Comprar</a>";
+                #echo "<li>".$producto . "<form action=".$eliminar."\" method=\"POST\"> <input id=\"eliminar\" type=\"submit\" value=\"Eliminar\"> </input> </form> <form action=".$comprar."\" method=\"POST\"> <input id=\"comprar\" type=\"submit\" value=\"Comprar\"></input> </form>";
+            }
+            else{
+                echo "Cesta vacía";
+            }
         }
-        else{
-            echo "Cesta vacía";
-        }
+        #echo "<a href=" .$comprar .">Comprar</a>";
+        #echo "<form action=".$comprar."\" method=\"POST\"> <input id=\"comprar\" type=\"submit\" value=\"Comprar\"></input> </form>";
+        echo "</ul>";
     }
-    #echo "<a href=" .$comprar .">Comprar</a>";
-    #echo "<form action=".$comprar."\" method=\"POST\"> <input id=\"comprar\" type=\"submit\" value=\"Comprar\"></input> </form>";
-    echo "</ul>";
+    else{
+        echo "Cesta vacía";
+    }
 }
 ?>
